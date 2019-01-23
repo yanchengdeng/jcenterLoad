8812c64a7355036cd67cf2a616468ee238b2ee9b
yancheng <yanchengdeng@gmail.com>
2019-01-23 15:54:51 +0800

    submit

---
+++ jcenter/src/main/java/api/yc/com/jcenter/CircleImage.java
@@ -0,0 +1,18 @@
+package api.yc.com.jcenter;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class CircleImage extends android.support.v7.widget.AppCompatImageView {
+    public CircleImage(Context context) {
+        super(context);
+    }
+
+    public CircleImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ jcenter/src/main/java/api/yc/com/jcenter/MainActivity.java
@@ -0,0 +1,13 @@
+package api.yc.com.jcenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
